c60501fb343589e60fe40afcc56b4811818dfb5a
SubhradeepSeth <139810175+SubhradeepSeth@users.noreply.github.com>
2024-01-05 16:36:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function makeid(l) {
   // write your code here
+	let res="";
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	let l2=characters.length;
+	for(let i=0;i<l;i++){
+		res+=characters.charAt(Math.floor(Math.random() * l2));
+	}
+	return res;
 }
-
 // Do not change the code below.
 const l = prompt("Enter a number.");
 alert(makeid(l));
